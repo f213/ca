f7b93c6b82cc42ac7e7bbcdbe405706b352d8f53
Fedor Borshev <fedor9@gmail.com>
2012-04-02 20:50:36 +0400

    Исправлена ошибка кодировки в файле для всплывающего кейпада

--- comp-admin/js/jquery.keypad-ru.js
+++ comp-admin/js/jquery.keypad-ru.js
@@ -1,10 +1,10 @@
-п»ї(function($) {
+﻿(function($) {
 
 $.keypad.regional['ru'] = {
-	buttonText: '...', buttonStatus: 'РћС‚РєСЂС‹С‚СЊ СЌРєСЂР°РЅРЅСѓСЋ РєР»Р°РІРёР°С‚СѓСЂСѓ',
-	closeText: 'Р—Р°РєСЂС‹С‚СЊ', closeStatus: 'Р—Р°РєСЂС‹С‚СЊ',
-	clearText: 'РћС‡РёСЃС‚РёС‚СЊ', clearStatus: 'РћС‡РёСЃС‚РёС‚СЊ РїРѕР»Рµ РІРІРѕРґР°',
-	backText: 'РќР°Р·Р°Рґ', backStatus: 'РЎС‚РµСЂРµС‚СЊ РїСЂРµРґС‹РґСѓС‰РёР№ СЃРёРјРІРѕР»',
+	buttonText: '...', buttonStatus: 'Открыть экранную клавиатуру',
+	closeText: 'Закрыть', closeStatus: 'Закрыть',
+	clearText: 'Очистить', clearStatus: 'Очистить поле ввода',
+	backText: 'Назад', backStatus: 'Стереть предыдущий символ',
 	alphabeticLayout: $.keypad.qwertzAlphabetic,
 	fullLayout: $.keypad.qwertzLayout,
 	isAlphabetic: $.keypad.isAlphabetic,
